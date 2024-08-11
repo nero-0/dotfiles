524bbd306d26ae7afe0e759fd8d9ac8822db452e
nero <kvshcz@gmail.com>
2024-08-11 16:52:02 -0300

    minor changes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm "$HOME/../usr/etc/motd"
-mv "termux/*" "$HOME/.termux/" && mv "config" "$HOME/.config" && rm -r "termux"
+mv termux/* "$HOME"/.termux/ && mv config "$HOME"/.config && rm -r termux
 pkg install neovim fish starship getconf which jq ripgrep -y
 chsh -s fish
